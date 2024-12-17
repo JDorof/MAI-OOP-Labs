bf94b1ee24fa87aafa92cdaaa30def8bc8ab8313
JDorof <demadorofeevicok977@gmail.com>
2024-12-18 01:40:59 +0300

    ready

---
+++ lab4/build/CMakeFiles/gtest_exe.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/gtest_exe.dir/main.cpp.o: \
+ /home/justdema/OOP/MAI-OOP-Labs/lab4/main.cpp /usr/include/stdc-predef.h
---
+++ lab4/build/CMakeFiles/gtest_lib.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/justdema/OOP/MAI-OOP-Labs/lab4/src/figure.cpp" "CMakeFiles/gtest_lib.dir/src/figure.cpp.o" "gcc" "CMakeFiles/gtest_lib.dir/src/figure.cpp.o.d"
+  "/home/justdema/OOP/MAI-OOP-Labs/lab4/src/figure_vector.cpp" "CMakeFiles/gtest_lib.dir/src/figure_vector.cpp.o" "gcc" "CMakeFiles/gtest_lib.dir/src/figure_vector.cpp.o.d"
+  "/home/justdema/OOP/MAI-OOP-Labs/lab4/src/pentagon.cpp" "CMakeFiles/gtest_lib.dir/src/pentagon.cpp.o" "gcc" "CMakeFiles/gtest_lib.dir/src/pentagon.cpp.o.d"
+  "/home/justdema/OOP/MAI-OOP-Labs/lab4/src/point.cpp" "CMakeFiles/gtest_lib.dir/src/point.cpp.o" "gcc" "CMakeFiles/gtest_lib.dir/src/point.cpp.o.d"
+  "/home/justdema/OOP/MAI-OOP-Labs/lab4/src/romb.cpp" "CMakeFiles/gtest_lib.dir/src/romb.cpp.o" "gcc" "CMakeFiles/gtest_lib.dir/src/romb.cpp.o.d"
+  "/home/justdema/OOP/MAI-OOP-Labs/lab4/src/trapezium.cpp" "CMakeFiles/gtest_lib.dir/src/trapezium.cpp.o" "gcc" "CMakeFiles/gtest_lib.dir/src/trapezium.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
