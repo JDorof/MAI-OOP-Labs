0249bbd3e94c3a1ee88d7b923089936217934961
JDorof <demadorofeevicok977@gmail.com>
2024-12-21 01:58:45 +0300

    finished with 7 T_T

---
+++ lab7/build/CMakeFiles/lab.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/justdema/OOP/MAI-OOP-Labs/lab7/src/entity.cpp" "CMakeFiles/lab.dir/src/entity.cpp.o" "gcc" "CMakeFiles/lab.dir/src/entity.cpp.o.d"
+  "/home/justdema/OOP/MAI-OOP-Labs/lab7/src/game.cpp" "CMakeFiles/lab.dir/src/game.cpp.o" "gcc" "CMakeFiles/lab.dir/src/game.cpp.o.d"
+  "/home/justdema/OOP/MAI-OOP-Labs/lab7/src/main.cpp" "CMakeFiles/lab.dir/src/main.cpp.o" "gcc" "CMakeFiles/lab.dir/src/main.cpp.o.d"
+  "/home/justdema/OOP/MAI-OOP-Labs/lab7/src/npc.cpp" "CMakeFiles/lab.dir/src/npc.cpp.o" "gcc" "CMakeFiles/lab.dir/src/npc.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab7/build/runTests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/justdema/OOP/MAI-OOP-Labs/lab7/build/runTests[1]_tests.cmake")
+  include("/home/justdema/OOP/MAI-OOP-Labs/lab7/build/runTests[1]_tests.cmake")
+else()
+  add_test(runTests_NOT_BUILT runTests_NOT_BUILT)
+endif()
